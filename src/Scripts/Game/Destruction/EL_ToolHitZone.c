5cb8180b8a9aa47725dc17d0cc18a1e3a7d5299b
TheBonBon <45827986+thebonbon@users.noreply.github.com>
2022-10-03 18:19:59 +0200

    Added ore shake + fixes

--- src/Scripts/Game/Destruction/EL_ToolHitZone.c
+++ src/Scripts/Game/Destruction/EL_ToolHitZone.c
@@ -1,24 +1,52 @@
 class EL_ToolHitZone : ScriptedHitZone
 {
 
-	[Attribute("", UIWidgets.ResourceNamePicker, "Particle to spawn on hit", "et")]
-	ResourceName m_HitVFX;
+	[Attribute(ResourceName.Empty, UIWidgets.ResourceNamePicker, "Particle to spawn on hit", "ptc")]
+	ResourceName m_HitParticle;
 	[Attribute("", UIWidgets.Slider, "Effective tool damage", "0.0 100.0 0.01")]
 	float m_fEffectiveDamage;
+	[Attribute("0.1", UIWidgets.Slider, "Shake intensity", "0.0 1 0.1")]
+	float m_fShakeIntensity;
+	[Attribute("3", UIWidgets.Slider, "Shake amount", "0.0 100 1.0")]
+	int m_iShakeAmount;
 
+	vector m_vOriginalPos;
+	
 	//------------------------------------------------------------------------------------------------
 	override float ComputeEffectiveDamage(EDamageType damageType, float rawDamage, IEntity hitEntity, HitZone struckHitZone, IEntity damageSource, IEntity damageSourceGunner, IEntity damageSourceParent, const GameMaterial hitMaterial, int colliderID, const inout vector hitTransform[3], const vector impactVelocity, int nodeID, bool isDOT)
 	{
-		//Wrong Tool
-		if (rawDamage !=  m_fEffectiveDamage) return 0;
+		//Check for tool
+		if (rawDamage != m_fEffectiveDamage) return 0;
+		
+		SCR_ParticleEmitter.Create(m_HitParticle, hitTransform[0], hitTransform[1]);
+		
+		//Shake "Animation"
+		m_vOriginalPos = GetOwner().GetOrigin();
+		Shake();
 		
 		return super.ComputeEffectiveDamage(damageType, rawDamage, hitEntity, struckHitZone, damageSource, damageSourceGunner, damageSourceParent, hitMaterial, colliderID, hitTransform, impactVelocity, nodeID, isDOT);
-	}
 
+			
+	}
+	
 	//------------------------------------------------------------------------------------------------
-	override void OnLocalDamage(EDamageType type, float damage, HitZone pOriginalHitzone, IEntity damageSource, IEntity instigator, inout vector hitTransform[3], float speed, int colliderID, int nodeID)
+	void Shake()
 	{
-		EL_Utils.SpawnEntityPrefab(m_HitVFX, hitTransform[0], hitTransform[1]);
+		bool negativeOffset;
+		for (int i=0; i < m_iShakeAmount; i++)
+		{
+			vector offset = vector.Right * m_fShakeIntensity;
+			if (negativeOffset)
+				offset *= -1;
+			negativeOffset = !negativeOffset;
+			GetGame().GetCallqueue().CallLater(SetNewPos, i * 50, false, m_vOriginalPos + offset);
+		}
+		GetGame().GetCallqueue().CallLater(SetNewPos, m_iShakeAmount * 50, false, m_vOriginalPos);
 	}
-
+	
+	//------------------------------------------------------------------------------------------------
+	void SetNewPos(vector pos)
+	{
+		GetOwner().SetOrigin(pos);
+	}
 };
