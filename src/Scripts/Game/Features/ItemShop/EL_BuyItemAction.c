f584e77dd6c923e257cd8884b9c60264e1ac7a84
Jasper <Kasperonpc@gmail.com>
2024-07-06 11:39:28 +0200

    Starting

--- src/Scripts/Game/Features/ItemShop/EL_BuyItemAction.c
+++ src/Scripts/Game/Features/ItemShop/EL_BuyItemAction.c
@@ -13,10 +13,24 @@ class EL_BuyItemAction : ScriptedUserAction
 
 		//Play pickup animation
 		CharacterControllerComponent controller = CharacterControllerComponent.Cast(pUserEntity.FindComponent(CharacterControllerComponent));
-		controller.TryPlayItemGesture(EItemGesture.EItemGesturePickUp);
+		if (controller)
+		{
+			controller.TryPlayItemGesture(EItemGesture.EItemGesturePickUp);
+		}
+		else
+		{
+			Print("Error: CharacterControllerComponent not found!", LogLevel.ERROR);
+			return;
+		}
 
 		SCR_InventoryStorageManagerComponent inventoryManager = SCR_InventoryStorageManagerComponent.Cast(pUserEntity.FindComponent(SCR_InventoryStorageManagerComponent));
-		for (int i; i < m_iBuyAmount; i++)
+		if (!inventoryManager)
+		{
+			Print("Error: SCR_InventoryStorageManagerComponent not found!", LogLevel.ERROR);
+			return;
+		}
+
+		for (int i = 0; i < m_iBuyAmount; i++)
 		{
 			EL_InventoryUtils.AddAmount(pUserEntity, m_BuyablePrefab, 1);
 			EL_MoneyUtils.RemoveCash(pUserEntity, m_ItemPriceConfig.m_iBuyPrice);
@@ -35,31 +49,20 @@ class EL_BuyItemAction : ScriptedUserAction
 
 	//------------------------------------------------------------------------------------------------
 	override bool CanBePerformedScript(IEntity user)
- 	{
+	{
 		if (!m_ItemPriceConfig || !m_BuyablePrefab)
 			return false;
 
 		//1. Check money
-		if(EL_MoneyUtils.GetCash(user) < m_ItemPriceConfig.m_iBuyPrice * m_iBuyAmount)
+		if (EL_MoneyUtils.GetCash(user) < m_ItemPriceConfig.m_iBuyPrice * m_iBuyAmount)
 		{
 			SetCannotPerformReason("Can't afford");
 			return false;
 		}
 
-		//2. Check free equipment slots
-		IEntityComponentSource clothComponentSource = SCR_BaseContainerTools.FindComponentSource(Resource.Load(m_BuyablePrefab), BaseLoadoutClothComponent);
-		if (clothComponentSource)
-		{
-			LoadoutAreaType areaType;
-			clothComponentSource.Get("AreaType", areaType);
-			BaseLoadoutManagerComponent loadoutManager = EL_ComponentFinder<BaseLoadoutManagerComponent>.Find(user);
-			if (loadoutManager.IsAreaAvailable(areaType.Type()))
-				return true;
-		}
-
 		//3. Check free inventory space
 		SCR_InventoryStorageManagerComponent inventoryManager = SCR_InventoryStorageManagerComponent.Cast(user.FindComponent(SCR_InventoryStorageManagerComponent));
-		if (!inventoryManager.CanInsertItem(GetOwner()))
+		if (!inventoryManager || !inventoryManager.CanInsertItem(GetOwner()))
 		{
 			SetCannotPerformReason("Inventory full");
 			return false;
@@ -70,7 +73,7 @@ class EL_BuyItemAction : ScriptedUserAction
 
 	//------------------------------------------------------------------------------------------------
 	override bool CanBeShownScript(IEntity user)
- 	{
+	{
 		return (
 			m_ItemPriceConfig &&
 			m_ItemPriceConfig.m_iBuyPrice != -1 &&
@@ -83,12 +86,43 @@ class EL_BuyItemAction : ScriptedUserAction
 	//------------------------------------------------------------------------------------------------
 	override void Init(IEntity pOwnerEntity, GenericComponent pManagerComponent)
 	{
+		if (!pOwnerEntity)
+		{
+			Print("Error: pOwnerEntity is null!", LogLevel.ERROR);
+			return;
+		}
+
 		EL_ShopItemComponent shopItemComponent = EL_ShopItemComponent.Cast(pOwnerEntity.FindComponent(EL_ShopItemComponent));
+		if (!shopItemComponent)
+		{
+			Print("Error: EL_ShopItemComponent not found!", LogLevel.ERROR);
+			return;
+		}
+
 		m_BuyablePrefab = shopItemComponent.GetShopItemPrefab();
 		m_ItemPriceConfig = shopItemComponent.GetShopItemPriceConfig();
 
-		InventoryItemComponent invItem = EL_ComponentFinder<InventoryItemComponent>.Find(pOwnerEntity);
-		invItem.SetAdditionalVolume(EL_PrefabUtils.GetPrefabItemVolume(m_BuyablePrefab));
-		invItem.SetAdditionalWeight(EL_PrefabUtils.GetPrefabItemWeight(m_BuyablePrefab));
+		if (!m_BuyablePrefab)
+		{
+			Print("Error: m_BuyablePrefab is null!", LogLevel.ERROR);
+			return;
+		}
+
+		if (!m_ItemPriceConfig)
+		{
+			Print("Error: m_ItemPriceConfig is null!", LogLevel.ERROR);
+			return;
+		}
+
+		InventoryItemComponent invItem = EL_Component<InventoryItemComponent>.Find(pOwnerEntity);
+		if (invItem)
+		{
+			invItem.SetAdditionalVolume(EL_PrefabUtils.GetPrefabItemVolume(m_BuyablePrefab));
+			invItem.SetAdditionalWeight(EL_PrefabUtils.GetPrefabItemWeight(m_BuyablePrefab));
+		}
+		else
+		{
+			Print("Error: InventoryItemComponent not found!", LogLevel.ERROR);
+		}
 	}
-}
+};
